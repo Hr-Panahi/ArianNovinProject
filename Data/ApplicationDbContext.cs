93d4d0daadf70f83ff13342315e40fbab1dae084
Hamid Reza Panahi <Hr.panahi@live.com>
2024-07-23 18:08:53 +0330

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ArianNovinProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArianNovinProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArianNovinProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
